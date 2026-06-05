429742bf47fda478108ae5174739948f5f8e75e2
Longvan <longvanasb@gmail.com>
2026-06-05 12:58:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -577,6 +577,7 @@ function renderTable() {
           <button class="link-btn" onclick="openMoveModal(${s.id})">Chuyển</button>
           <button class="link-btn" onclick="openPartialExportModal(${s.id})">Xuất một phần</button>
           <button class="link-btn danger" onclick="markExported(${s.id})">Xuất hết</button>
+          <button class="link-btn danger" onclick="markExported(${s.id})">Xuất hết</button>
         `
         : `<span class="muted-text">Chỉ xem</span>`;
 
